431684e8e442c58bc5a716cb81099e8fd23d1688
Andrey Volkov <andrey.dreamwall@gmail.com>
2025-02-10 19:24:39 +0400

    hello

---
+++ hello/hello.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+const (
+	russian = "Russian"
+	french  = "French"
+	spanish = "Spanish"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+	russianHelloPrefix = "Privet, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case russian:
+		prefix = russianHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main() {
+	fmt.Println(Hello("World", ""))
+}
